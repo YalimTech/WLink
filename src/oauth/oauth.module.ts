df0f615913d1f9fd9a7e739fb41bb3a518ee84ee
YC <30573421+YalimTech@users.noreply.github.com>
2025-07-10 16:33:07 -0400

    Update oauth.module.ts

--- src/oauth/oauth.module.ts
+++ src/oauth/oauth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from "@nestjs/common";
 import { GhlOauthController } from "./oauth.controller";
+import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from "../prisma/prisma.service";
+import { GhlOAuthCallbackDto } from "./dto/ghl-oauth-callback.dto";
 
 @Module({
-	controllers: [GhlOauthController],
+  imports: [ConfigModule], // para poder usar ConfigService
+  controllers: [GhlOauthController],
+  providers: [PrismaService],
 })
 export class OauthModule {}
