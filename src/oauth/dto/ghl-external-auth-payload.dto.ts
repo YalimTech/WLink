bf6b482c07bd422d9886aa91783ff999dc8a6060
YC <30573421+YalimTech@users.noreply.github.com>
2025-07-13 18:21:39 -0400

    Update ghl-external-auth-payload.dto.ts

--- src/oauth/dto/ghl-external-auth-payload.dto.ts
+++ src/oauth/dto/ghl-external-auth-payload.dto.ts
@@ -1,31 +1,27 @@
 import {
   IsString,
   IsNotEmpty,
-  IsArray,
-  ArrayNotEmpty,
   Matches,
-  ValidateIf,
+  IsOptional,
 } from 'class-validator';
 
 export class GhlExternalAuthPayloadDto {
-  @IsArray()
-  @ArrayNotEmpty()
-  @IsString({ each: true })
-  locationId: string[];
-
-  @ValidateIf((_, value) => value !== undefined)
   @IsString()
   @IsNotEmpty()
+  locationId: string;
+
+  @IsOptional()
+  @IsString()
   @Matches(/^[a-zA-Z0-9\-]+$/, {
     message: 'instance_id must contain only letters, numbers, or dashes',
   })
-  instance_id: string;
+  instance_id?: string;
 
-  @ValidateIf((_, value) => value !== undefined)
+  @IsOptional()
   @IsString()
-  @IsNotEmpty()
   @Matches(/^[a-zA-Z0-9\-]+$/, {
     message: 'api_token_instance must contain only letters, numbers, or dashes',
   })
-  api_token_instance: string;
+  api_token_instance?: string;
 }
+
