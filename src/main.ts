c48464e053dcbb4d571ac9570327207041b91e7b
YC <30573421+YalimTech@users.noreply.github.com>
2025-07-13 20:21:30 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-import { settings } from "./.evolutionapi";
+//import { settings } from "./.evolutionapi";
 
 declare global {
   namespace PrismaJson {
