1da68e01ec96803946b7c6aa3d29cf955239cff5
YC <30573421+YalimTech@users.noreply.github.com>
2025-07-13 20:27:28 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
-//import { settings } from "./.evolutionapi";
-
-declare global {
-  namespace PrismaJson {
-    // noinspection JSUnusedGlobalSymbols
-    type InstanceSettings = typeof settings;
-  }
-}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn', 'log', 'debug', 'verbose'],
-  });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }));
-  app.use(helmet());
-  app.enableShutdownHooks();
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {});
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true, forbidNonWhitelisted: true }));
+    app.use(helmet());
+    app.enableShutdownHooks();
+    await app.listen(3000);
 }
-
 void bootstrap();
 
