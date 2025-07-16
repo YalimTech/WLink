81c6b520c80efb24c0db0cc42395d72c14add7b7
YC <30573421+YalimTech@users.noreply.github.com>
2025-07-16 00:19:45 -0400

    Update index.ts

--- src/evolutionapi/index.ts
+++ src/evolutionapi/index.ts
@@ -1,39 +1,46 @@
+// src/evolutionapi/index.ts
+
+import { User, Instance, InstanceState, UserCreateData, UserUpdateData } from '../types';
+import { Prisma } from '@prisma/client';
+
 export interface Settings {
-  wid?: string;
   [key: string]: any;
 }
 
-export interface StorageProvider<User, Instance, UserCreateData, UserUpdateData> {
-  createUser(data: UserCreateData): Promise<User>;
-  findUser(identifier: string): Promise<User | null>;
-  updateUser(identifier: string, data: UserUpdateData): Promise<User>;
-  getUserWithTokens(userId: string): Promise<User | null>;
-  updateUserTokens(userId: string, accessToken: string, refreshToken: string, tokenExpiresAt: Date): Promise<User>;
-  createInstance(data: any): Promise<Instance>;
-  getInstance(idInstance: string): Promise<Instance | null | (Instance & { user: User })>;
-  getInstancesByUserId(userId: string): Promise<Instance[]>;
-  removeInstance(idInstance: string): Promise<Instance>;
-  updateInstanceSettings(idInstance: string, settings: Settings): Promise<Instance>;
-  updateInstanceState(idInstance: string, state: any): Promise<Instance>;
-  updateInstanceName(idInstance: string, name: string): Promise<Instance & { user: User }>;
+// --- Interfaces de Almacenamiento (StorageProvider) ---
+export interface StorageProvider<U, V, C, D> {
+  // Métodos de Usuario
+  createUser(data: C): Promise<U>;
+  findUser(identifier: string): Promise<U | null>;
+  updateUser(identifier: string, data: D): Promise<U>;
+
+  // Métodos de Instancia
+  createInstance(data: any): Promise<V>;
+  getInstance(idInstance: string): Promise<V | null>;
+  getInstancesByUserId(userId: string): Promise<V[]>;
+  removeInstance(idInstance: string): Promise<V>;
+  updateInstanceName(idInstance: string, name: string): Promise<V>;
+  updateInstanceState(idInstance: string, state: InstanceState): Promise<V>;
+  updateInstanceSettings(idInstance: string, settings: Settings): Promise<V>;
 }
 
-import { Logger } from '@nestjs/common';
+// --- Interfaces de Transformación de Mensajes ---
+export interface MessageTransformer<T, U> {
+  toPlatformMessage(payload: U): T;
+  fromPlatformMessage(message: T): any;
+}
 
-export class EvolutionApiLogger extends Logger {
-  static getInstance(context: string): EvolutionApiLogger {
-    return new EvolutionApiLogger(context);
+// --- Interfaces de Errores Personalizados ---
+export class NotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'NotFoundError';
   }
 }
 
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-
-export abstract class BaseEvolutionApiAuthGuard implements CanActivate {
-  constructor(protected readonly storageService: StorageProvider<any, any, any, any>) {}
-  async canActivate(_context: ExecutionContext): Promise<boolean> {
-    return true;
-  }
-  protected async validateRequest(_request: any): Promise<boolean> {
-    return true;
+export class IntegrationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'IntegrationError';
   }
 }
